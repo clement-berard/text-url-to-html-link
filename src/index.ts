1fff8c9d014ff48632ae7e2f98f52cada17a6a8d
Clément BERARD <38923547+clement-berard@users.noreply.github.com>
2020-11-14 14:11:05 +0100

    feat(regex): update regex to support DN with one char (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const regexFull = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-const regexLight = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+const regexFull = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+const regexLight = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
 export const textUrlToHtmlLink = (text: string, attrs: object = {}, restrictive: boolean = true) => {
     let attrsStr = '';
